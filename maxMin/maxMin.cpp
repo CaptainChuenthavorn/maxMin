85335e0e20d57a942a7daad78e6465cf31b7d0b9
จักรภัทรณ์ ชื่นถาวร <captainchuenthavorn@hotmail.com>
2020-09-16 22:44:38 +0700

    complete

--- maxMin/maxMin.cpp
+++ maxMin/maxMin.cpp
@@ -6,22 +6,18 @@ int min(int* ,int );
 int main() {
 	int input;
 	int* p;
+	printf("How many numbers you want to compare ? : ");
 	scanf("%d", &input);
 	p = (int*)malloc(sizeof(int));
+	printf("What are they ? : ");
 	for (int i = 0;i < input;i++) {
 		scanf("%d",  &p[i]);
 	}
-	printf("P : %d ",input);
-
-	printf("Number : ");
-	for (int i = 0;i < input;i++) {
-		printf(" %d ", p[i]);
-	}
-	printf("\nMAX : %d\nMIN : %d", max(p,input), min(p, input));
+	printf("\nMAX number is : %d\nMIN number is : %d", max(p,input), min(p, input));
 	return 0;
 }
 int max(int *p, int input) {
-	int max=0;
+	int max= -214748364;
 	for (int i = 0;i < input;i++) {
 		if (p[i] > max) {
 			max = p[i];
@@ -30,7 +26,7 @@ int max(int *p, int input) {
 	return max;
 }
 int min(int* p,int input) {
-	int min = 200000;
+	int min = 214748364;
 	for (int i = 0;i < input;i++) {
 		if (p[i] < min) {
 			min = p[i];
