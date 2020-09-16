0c74b6f46c09cd28118bb00215e967d610ef9b08
จักรภัทรณ์ ชื่นถาวร <captainchuenthavorn@hotmail.com>
2020-09-16 22:15:52 +0700

    logic complete

--- maxMin/maxMin.cpp
+++ maxMin/maxMin.cpp
@@ -1,8 +1,8 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <stdlib.h>
-int max(int* );
-int min(int* );
+int max(int* ,int );
+int min(int* ,int );
 int main() {
 	int input;
 	int* p;
@@ -11,28 +11,27 @@ int main() {
 	for (int i = 0;i < input;i++) {
 		scanf("%d",  &p[i]);
 	}
+	printf("P : %d ",input);
+
 	printf("Number : ");
 	for (int i = 0;i < input;i++) {
 		printf(" %d ", p[i]);
 	}
-	max(p);
-	printf("\nMAX : %d\nMIN : %d", max(p), min(p));
+	printf("\nMAX : %d\nMIN : %d", max(p,input), min(p, input));
 	return 0;
 }
-int max(int *p) {
-	int max=0,min=2000;
-	for (int i = 0;i < sizeof(*p);i++) {
+int max(int *p, int input) {
+	int max=0;
+	for (int i = 0;i < input;i++) {
 		if (p[i] > max) {
 			max = p[i];
 		}
 	}
 	return max;
-
 }
-int min(int* p) {
-	int max = 0, min = 200000;
-	for (int i = 0;i < sizeof(*p);i++) {
-		
+int min(int* p,int input) {
+	int min = 200000;
+	for (int i = 0;i < input;i++) {
 		if (p[i] < min) {
 			min = p[i];
 		}
